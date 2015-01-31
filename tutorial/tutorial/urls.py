4064ac51bfd3d01389df5aca66ad8bfd4b54b048
chenkuochen <chenkuochen.tw@gmail.com>
2015-01-31 18:29:25 +0800

    Integrate font-end and back-end

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from django.contrib import admin
 from quickstart import views
 from ui import views as ui_views
 from restfulAPI import views as restful_views
 from rest_framework import routers
+from django.conf.urls.static import static
+
 
 router = routers.DefaultRouter()
 router.register(r'tables', restful_views.TableViewSet)
@@ -11,7 +14,8 @@ router.register(r'meals', restful_views.MealViewSet)
 
 urlpatterns = patterns('',
     url(r'^$', ui_views.index),
+    url(r'^results$', ui_views.results),
     url(r'^api/', include(router.urls)),
     #url(r'^api/', restful_views),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-)
+) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- tutorial/ui/views.py
+++ tutorial/ui/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'ui/index.html')
+def results(request):
+    return render(request, 'ui/results.html')
